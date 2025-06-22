4a06f9b42f397a2b624e8c2755e521a94f4ead00
yasersyafa <syafayaser@gmail.com>
2025-06-22 15:31:51 +0700

    Add: Hero Section in home

---
+++ src/partials/FeatureSection.tsx
@@ -0,0 +1,9 @@
+const FeatureSection = () => {
+    return (
+        <section className="w-full h-screen pt-32 text-[#2D4F2B]">
+            <h1 className="font-bold text-xl md:text-3xl lg:text-6xl">Our Key Features</h1>
+        </section>
+    )
+}
+
+export default FeatureSection
--- src/partials/HeroSection.tsx
+++ src/partials/HeroSection.tsx
@@ -1,6 +1,14 @@
+import { Button } from "@/components/ui/button"
+
 const HeroSection = () => {
     return (
-        <div></div>
+        <section className="w-full h-screen flex flex-col items-center justify-center gap-10 bg-[#FFE3BB] text-[#2D4F2B]">
+            <h1 className="text-xl md:text-3xl lg:text-5xl xl:text-8xl font-bold max-w-xl sm:max-w-2xl md:max-w-5xl text-center">Healthy Meals, Anytime, Anywhere</h1>
+            {/* description */}
+            <span className="font-light text-3xl">customizable healthy meal service with delivery all across Indonesia.</span>
+            {/* cta */}
+            <Button size={"lg"} className="bg-[#2D4F2B] hover:bg-[#708A58] text-2xl font-light">See Menu!</Button>
+        </section>
     )
 }
 
