9890dfd5b27fd76b10804dd922336e9e9eabab55
yasersyafa <syafayaser@gmail.com>
2025-06-23 14:52:05 +0700

    Update: Feature Section

--- src/partials/FeatureSection.tsx
+++ src/partials/FeatureSection.tsx
@@ -8,23 +8,26 @@ const FeatureSection = () => (
         </div>
         {/* grid view for displaying key features */}
         <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-3.5 px-20">
-            <div className="border-2 border-[#2D4F2B] h-fit min-h-[200px] flex flex-col items-center justify-center gap-3.5 rounded-xl p-5 text-center">
+            <div className="border-2 border-[#2D4F2B] h-full hover:scale-105 bg-white transition-all min-h-[200px] flex flex-col items-center justify-center gap-3.5 rounded-xl p-5 text-center">
                 {/* icon */}
                 <Salad size={100} />
                 {/* title card */}
                 <h1 className="text-2xl font-bold">Meal Customization</h1>
+                <p className="text-muted-foreground text-xl text-center">Choose meals that suit your taste, dietary needs, and lifestyle. From calorie-specific plans to vegetarian or protein-rich options, you are in full control of your daily menu.</p>
             </div>
-            <div className="border-2 border-[#2D4F2B] h-fit min-h-[200px] flex flex-col items-center justify-center gap-3.5 rounded-xl p-5 text-center">
+            <div className="border-2 border-[#2D4F2B] h-full hover:scale-105 bg-white transition-all min-h-[200px] flex flex-col items-center justify-center gap-3.5 rounded-xl p-5 text-center">
                 {/* icon */}
                 <Truck size={100} />
                 {/* title card */}
                 <h1 className="text-2xl font-bold">Delivery To Major Cities</h1>
+                <p className="text-muted-foreground text-xl text-center">We deliver fresh, ready-to-eat meals straight to your doorstep — now available in major cities across the country. Reliable, on-time, and always fresh.</p>
             </div>
-            <div className="border-2 border-[#2D4F2B] h-fit min-h-[200px] flex flex-col items-center justify-center gap-3.5 rounded-xl p-5 text-center">
+            <div className="border-2 border-[#2D4F2B] h-full hover:scale-105 bg-white transition-all min-h-[200px] flex flex-col items-center justify-center gap-3.5 rounded-xl p-5 text-center">
                 {/* icon */}
                 <SquareActivity size={100} />
                 {/* title card */}
                 <h1 className="text-2xl font-bold">Detailed Nutrition Information</h1>
+                <p className="text-muted-foreground text-xl text-center">Stay informed and in control of your health. Every meal comes with complete nutrition facts, including calories, macros, and ingredients — so you know exactly what you're eating.</p>
             </div>
         </div>
     </section>
