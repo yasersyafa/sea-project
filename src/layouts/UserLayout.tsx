70cfb6f29118fd111ea04ce810d4bd81231970da
Yaser Syafa <yasersyafaa@gt.student.pens.ac.id>
2025-06-25 14:48:00 +0700

    Add: toastify

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -3,11 +3,25 @@ import FooterSection from "@/partials/FooterSection"
 import NavigationBar from "@/partials/NavigationBar"
 import { useState } from "react"
 import { Outlet } from "react-router"
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/ReactToastify.css'
 
 const UserLayout = () => {
     const [isVisible, setIsVisible] = useState(true)
     return (
         <>
+            {/* Toast container agar global */}
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={true}
+                closeOnClick
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
             <NavigationBar onContactClicked={() => setIsVisible(true)} />
             <Outlet />
             <ContactBanner onClose={() => setIsVisible(false)} isVisible={isVisible} />
