d1d4dc4e3df73fd596a9dd4e888e821191afe55c
Yaser Syafa <yasersyafaa@gt.student.pens.ac.id>
2025-06-25 15:17:57 +0700

    Add: useAuth custom hook

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,67 @@
+import { useNavigate } from "react-router"
+import { toast } from "react-toastify"
+import api from "@/api/api"
+import { jwtDecode } from 'jwt-decode'
+
+export interface JwtPayload {
+    id: number,
+    name: string,
+    email: string,
+    role: 'user' | 'admin',
+    exp: number,
+    iat: number
+}
+
+export function useAuth() {
+    const navigate = useNavigate()
+
+    const login = async (email: string, password: string) => {
+        try {
+            const res = await api.post('/users/login', { email, password })
+            localStorage.setItem('token', res.data.token)
+            toast.success('Login successful')
+            navigate('/')
+        } catch (err: any) {
+            toast.error(err.response?.data?.message || 'Login failed')
+        }
+    }
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        toast.info('Logged out')
+        navigate('/')
+    }
+
+    const getToken = () => {
+        return localStorage.getItem('token')
+    }
+
+    const getUser = (): JwtPayload | null => {
+        const token = getToken()
+        if(!token) return null
+
+        try {
+            const decoded = jwtDecode<JwtPayload>(token)
+
+            if(decoded.exp * 1000 < Date.now()) {
+                logout()
+                return null
+            }
+
+            return decoded
+        } catch {
+            logout()
+            return null
+        }
+    }
+
+    const isLoggedIn = () => !!getUser()
+
+    return {
+        login,
+        logout,
+        getUser,
+        getToken,
+        isLoggedIn
+    }
+}
